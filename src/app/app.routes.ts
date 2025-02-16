2d0b99da46821e5924c4de0e8aadc0cdb063c584
irin <irin@IT-IRIN>
2025-02-16 11:50:12 +0600

    Removed previous component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,4 @@
 import { Routes } from '@angular/router';
-import { CreateAccountPageComponent } from './pages/create-account-page/create-account-page.component';
-import {NidVerificationComponent} from './pages/nid-verification/nid-verification.component';
-import { BeforeLoginNidComponent } from './pages/before-login-nid/before-login-nid.component';
-import { SuccessfulAccountComponent } from './pages/successful-account/successful-account.component';
-import { AuthGuard } from './guards/auth.guard';
 import { CommunicationComponent } from './pages/communication/communication.component';
 import { EmailTemplateComponent } from './pages/email-template/email-template.component';
 import { TemplateViewerComponent } from './pages/template-viewer/template-viewer.component';
@@ -18,24 +13,7 @@ export const routes: Routes = [
         redirectTo: 'communication',
         pathMatch: 'full'
     },
-    {
-        path:'register',
-        component: CreateAccountPageComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path: "account-created-successfully",
-        component: SuccessfulAccountComponent,
-        canActivate: [AuthGuard]
-    },
-    {
-        path:'register/nidVerify',
-        component: NidVerificationComponent
-    },
-    {
-        path:'register/before-login-nid',
-        component: BeforeLoginNidComponent
-    },
+  
     {
         path:'communication',
         component: CommunicationComponent,
